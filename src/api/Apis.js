5e3fd9288502eda869ac4853bd44b1cbcd918556
Daniel kebede <119158535+danielkeb@users.noreply.github.com>
2024-12-14 15:04:33 -0500

    Update Apis.js

--- src/api/Apis.js
+++ src/api/Apis.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://vms-server-production.up.railway.app';
 
 
 console.log('Environment:', process.env);
